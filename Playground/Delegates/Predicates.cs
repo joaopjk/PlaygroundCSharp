b227fabfeceb6cab75515a6006a3571bf3d91710
joaopjk <joaopjk48@gmail.com>
2021-07-04 21:18:56 -0300

    Multicast Delegate

---
+++ Playground/Delegates/Predicates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{/*
+  */
+    class Predicates
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
