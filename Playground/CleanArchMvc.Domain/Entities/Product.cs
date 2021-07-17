6b719dbbd5d7bb257093e52f2b3fe82d487ec805
joaopjk <joaopjk48@gmail.com>
2021-07-17 17:11:54 -0300

    Enriquecendo o modelo de domínio Category

--- Playground/CleanArchMvc.Domain/Entities/Product.cs
+++ Playground/CleanArchMvc.Domain/Entities/Product.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CleanArchMvc.Domain.Entities
+﻿namespace CleanArchMvc.Domain.Entities
 {
     public class Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public string Image { get; set; }
-        public int CategoryId { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+        public int CategoryId { get; private set; }
         public Category Category { get; set; }
     }
 }
