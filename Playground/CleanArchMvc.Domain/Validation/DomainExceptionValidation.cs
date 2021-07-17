eee0bcde3981f547fb4b0074f5005e07f0858f0d
joaopjk <joaopjk48@gmail.com>
2021-07-17 17:48:19 -0300

    Category Unit Test

--- Playground/CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
+++ Playground/CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
@@ -2,10 +2,10 @@
 
 namespace CleanArchMvc.Domain.Validation
 {
-    public class DomainExceptionValidation:Exception
+    public class DomainExceptionValidation : Exception
     {
         public DomainExceptionValidation(string error) : base(error) { }
-        public static void When(bool hasError,string error)
+        public static void When(bool hasError, string error)
         {
             if (hasError)
                 throw new DomainExceptionValidation(error);
