ca7fb42d6bc974b0e50d0d727f1413cea9986910
joaopjk <joaopjk48@gmail.com>
2021-07-27 20:26:31 -0300

    CategoryConfiguration

---
+++ Playground/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(p => p.Id);
+        }
+    }
+}
