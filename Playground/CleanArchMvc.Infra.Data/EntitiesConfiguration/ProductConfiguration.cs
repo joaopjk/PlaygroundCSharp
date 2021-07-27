35eb80bd2be4fa4f2397878222fdc1af92800386
joaopjk <joaopjk48@gmail.com>
2021-07-27 20:29:29 -0300

    ProductConfiguration

--- Playground/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
+++ Playground/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -4,11 +4,19 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
 {
-    public class ProductConfiguration : IEntityTypeConfiguration<Category>
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
-        public void Configure(EntityTypeBuilder<Category> builder)
+        public void Configure(EntityTypeBuilder<Product> builder)
         {
             builder.HasKey(p => p.Id);
+            builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+            builder.Property(c => c.Description).HasMaxLength(200).IsRequired();
+            builder.Property(c => c.Price).HasPrecision(10, 2).IsRequired();
+
+            builder
+                .HasOne(p => p.Category)
+                .WithMany(p => p.Products)
+                .HasForeignKey(p => p.CategoryId);
         }
     }
 }
