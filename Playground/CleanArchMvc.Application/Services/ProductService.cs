f7eb575cd8e34e1141ac7beec1821cd7c922f736
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2021-07-28 15:17:19 -0300

    ProductService

---
+++ Playground/CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task Add(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _repository.CreateAsync(productEntity);
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+            var productEntity = await _repository.GetByIdAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductCategory(int? id)
+        {
+            var productEntity = await _repository.GetProductCategoryAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productsEntity = await _repository.GetProductsAsync();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var productEntity =  _repository.GetByIdAsync(id).Result;
+            await _repository.RemoveAsync(productEntity);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _repository.UpdateAsync(productEntity);
+        }
+    }
+}
