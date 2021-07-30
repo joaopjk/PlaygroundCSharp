1d2e39c0b79248870809d354867d36558b1a5907
joaopjk <joaopjk48@gmail.com>
2021-07-29 23:38:54 -0300

    CategoriesController get

---
+++ Playground/CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _service;
+
+        public CategoriesController(ICategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _service.GetCategories();
+
+            if (categories == null)
+                return NotFound("Categories not found");
+            return Ok(categories);
+        }
+    }
+}
