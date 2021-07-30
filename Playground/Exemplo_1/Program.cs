a09e54818748dde590ebea99e272c98f49ce066b
joaopjk <joaopjk48@gmail.com>
2021-07-30 00:17:56 -0300

    GraphQL Exemplo 1

---
+++ Playground/Exemplo_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace Exemplo_1
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            Uri _baseAddress = new Uri("http://localhost:60064/");
+            HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(_baseAddress);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+        }
+    }
+}
