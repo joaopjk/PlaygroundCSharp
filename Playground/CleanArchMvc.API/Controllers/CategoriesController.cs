177c6a36f1f99fd383515fbab46a387acb3554a8
joaopjk <joaopjk48@gmail.com>
2021-07-29 23:41:33 -0300

    CategoriesController GetById

--- Playground/CleanArchMvc.API/Controllers/CategoriesController.cs
+++ Playground/CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -1,10 +1,7 @@
 ﻿using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.API.Controllers
