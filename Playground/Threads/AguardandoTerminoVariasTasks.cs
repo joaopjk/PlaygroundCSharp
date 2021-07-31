2e2383ae0426e8e9806d8f66023c5a33b10cf545
joaopjk <joaopjk48@gmail.com>
2021-07-31 19:59:12 -0300

    Aguardando o término de várias tasks

---
+++ Playground/Threads/AguardandoTerminoVariasTasks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class AguardandoTerminoVariasTasks
+    {
+        static void Main(string[] _)
+        {
+            Task[] tarefas = new Task[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                int numeroCorredor = i;
+                tarefas[i] = Task.Run(() => Correr(numeroCorredor));
+            }
+
+            Task.WaitAll(tarefas);
+            Console.WriteLine("Término do processamento. Tecle [ENTER] para terminar.");
+            Console.ReadLine();
+        }
+
+        public static void Correr(int numeroCorredor)
+        {
+            Console.WriteLine("Corredor {0} largou", numeroCorredor);
+            Thread.Sleep(1000);
+            Console.WriteLine("Corredor {0} terminou", numeroCorredor);
+        }
+    }
+}
