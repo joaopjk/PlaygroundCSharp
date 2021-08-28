2b2a5eb6a013b9c05218fca0a3af2cb7d5d6c358
joaopjk <joaopjk48@gmail.com>
2021-08-28 16:34:00 -0300

    Critical session

---
+++ Playground/2_DataSharing_Synchronization/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace _2_DataSharing_Synchronization
+{
+    internal class Program
+    {
+        class BackAccount
+        {
+            public int Balance { get; private set; }
+            private readonly object _padlock = new object();
+            public BackAccount(int balance)
+            {
+
+                Balance = balance;
+            }
+
+            public void Deposit(int amount)
+            {
+                lock (_padlock)
+                {
+                    Balance += amount;
+                }
+            }
+
+            public void Withdraw(int amount)
+            {
+                lock (_padlock)
+                {
+                    Balance -= amount;
+                }
+            }
+        }
+
+        private static void Main()
+        {
+            //Critical Sections => One method can be access the area at a time
+            var tasks = new List<Task>();
+            BackAccount ba = new(0);
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        ba.Deposit(100);
+                    }
+                }));
+
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        ba.Withdraw(100);
+                    }
+                }));
+
+                Task.WaitAll(tasks.ToArray());
+            }
+
+            Console.WriteLine($"Final Balance {ba.Balance}");
+        }
+    }
+}
