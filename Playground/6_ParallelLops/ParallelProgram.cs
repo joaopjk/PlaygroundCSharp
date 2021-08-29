0215b9ede6de0e5e21df056436309d9b66356401
joaopjk <joaopjk48@gmail.com>
2021-08-28 23:10:50 -0300

    Parallel

---
+++ Playground/6_ParallelLops/ParallelProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _6_ParallelLops
+{
+    class ParallelProgram
+    {
+        static void Main(string[] args)
+        {
+            int[] values = new int[100];
+            var a = new Action(() => Console.WriteLine($"First {Task.CurrentId}"));
+            var b = new Action(() => Console.WriteLine($"Second {Task.CurrentId}"));
+            var c = new Action(() => Console.WriteLine($"Third {Task.CurrentId}"));
+
+            Parallel.Invoke(a,b,c);
+        }
+    }
+}
