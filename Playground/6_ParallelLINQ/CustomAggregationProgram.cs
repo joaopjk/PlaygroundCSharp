61931cbd1ca4f3d40ad98f39dda1ec0c7e4fbaa4
joaopjk <joaopjk48@gmail.com>
2021-08-29 09:29:03 -0300

    Custom Aggregations

--- Playground/6_ParallelLINQ/CustomAggregationProgram.cs
+++ Playground/6_ParallelLINQ/CustomAggregationProgram.cs
@@ -7,8 +7,18 @@ namespace _6_ParallelLINQ
     {
         public static void Main(string[] args)
         {
-            var sum = Enumerable.Range(1, 1001).Sum();
-            Console.WriteLine(sum);
+            //var sum = Enumerable.Range(1, 1001).Sum();
+            //Same value of .Sum
+            //var sum = Enumerable.Range(1, 1000)
+            //    .Aggregate(0, (i, j) => i + j);
+            var sum = ParallelEnumerable.Range(1, 1000)
+                .Aggregate(
+                    0, //Starting value
+                    (partialSum, i) => partialSum += i,
+                    (total, subtotal) => total += subtotal,
+                    i => 1);
+
+            Console.WriteLine($"sum = {sum}");
         }
     }
 }
