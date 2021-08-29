c904158afa222a60f501e37f8c38c3c9b9622a65
joaopjk <joaopjk48@gmail.com>
2021-08-29 09:28:35 -0300

    Custom Aggregations

---
+++ Playground/6_ParallelLINQ/CustomAggregationProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _6_ParallelLINQ
+{
+    class CustomAggregationProgram
+    {
+        public static void Main(string[] args)
+        {
+            var sum = Enumerable.Range(1, 1001).Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
