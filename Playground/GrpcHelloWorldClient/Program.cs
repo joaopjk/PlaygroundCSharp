05808aca8a2a17aba8d566cae9e2d7d766c8d47f
joaopjk <joaopjk48@gmail.com>
2021-09-01 10:32:51 -0300

    Checkpoint

--- Playground/GrpcHelloWorldClient/Program.cs
+++ Playground/GrpcHelloWorldClient/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcHelloWorldClient.Protos;
 
 namespace GrpcHelloWorldClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new HelloService.HelloServiceClient(channel);
+
+            var reply = await client.SayHelloAsync(
+                    new HelloRequest { Name = "Hello SWN Client" }
+                );
+
+            Console.WriteLine("Greetings: " + reply.Message);
         }
     }
 }
