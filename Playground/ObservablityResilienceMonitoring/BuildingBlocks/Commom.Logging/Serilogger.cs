204c306dd36cb6b9e450435179041d5c32f8ec7c
joaopjk <joaopjk48@gmail.com>
2021-09-11 16:22:21 -0300

    Create Serilog common logging class library for ElasticSearch and Kibana

---
+++ Playground/ObservablityResilienceMonitoring/BuildingBlocks/Commom.Logging/Serilogger.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Reflection;
+
+namespace Commom.Logging
+{
+    public class Serilogger
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
+             (context, configuration) =>
+            {
+                var elasticUri = context.Configuration["ElasticConfiguration:Uri"];
+                configuration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .WriteTo.Console()
+                    .WriteTo.Elasticsearch(
+                        new ElasticsearchSinkOptions(
+                                new Uri(elasticUri))
+                        {
+                            IndexFormat =
+                                $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-" +
+                                $"{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-" +
+                                $"logs-{DateTime.UtcNow:yyyy-MM-dd}",
+                            AutoRegisterTemplate = true,
+                            NumberOfShards = 2,
+                            NumberOfReplicas = 1
+                        }
+                    )
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                    .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                    .ReadFrom.Configuration(context.Configuration);
+            };
+    }
+}
