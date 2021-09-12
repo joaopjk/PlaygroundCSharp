5adfaaa557989d5b464b9105f7fb9347f43d6406
joaopjk <joaopjk48@gmail.com>
2021-09-12 15:52:35 -0300

    Implemenation of Banking Domain

---
+++ Playground/MicroRabbit.Banking.Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Banking.Domain.Interfaces
+{
+    interface IAccountRepository
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}
