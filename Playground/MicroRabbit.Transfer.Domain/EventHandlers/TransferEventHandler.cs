600a9d3248aedcc0161ae2fbef6e775e9566e8ff
joaopjk <joaopjk48@gmail.com>
2021-09-12 22:02:29 -0300

    Update dependency container

---
+++ Playground/MicroRabbit.Transfer.Domain/EventHandlers/TransferEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Transfer.Domain.Events;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Transfer.Domain.EventHandlers
+{
+    public class TransferEventHandler : IEventHandler<TransferCreatedEvent>
+    {
+        public TransferEventHandler() { }
+        public Task Handle(TransferCreatedEvent @event)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
