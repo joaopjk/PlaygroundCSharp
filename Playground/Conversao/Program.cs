f9b85b93c37479a01b51dd9a7a9aedb847139962
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2021-09-13 14:47:14 -0300

    Conversão implícita e explícita

---
+++ Playground/Conversao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conversao
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            //Conversão implicita
+            float x = 4.5f;
+            double y = x;
+
+            //Conversão explicita
+            x = (float)y;
+
+            int a = 5;
+            int b = 2;
+            double resultado = (double)a / b;
+
+            Console.WriteLine(resultado);
+            Console.WriteLine(x);
+        }
+    }
+}
