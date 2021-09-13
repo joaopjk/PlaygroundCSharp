91b3c5bdb810ce28844098379568de416cf888ee
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2021-09-13 08:06:55 -0300

    Tipos primitivos

---
+++ Playground/Tipos/Basicos.cs
@@ -0,0 +1,22 @@
+﻿namespace Tipos
+{
+    public class Basicos
+    {
+        static void Main(string[] _)
+        {
+            sbyte sb = 100; //-128 a 127 SByte
+            short sh = 100;// Int16
+            int inteiro = 100;//Int32
+            long longo = 10000;//Int64
+            byte by = 1;//Byte
+            ushort us = 1; //Uint16
+            uint ui = 1; //Uint32
+            ulong ul = 10000; // Uint64
+            float fl = 2.64f;//Single
+            double dl = 4.45;//Double
+            decimal dc = 0;// Decimal
+            char c = 'a';//Char
+            bool bo = true;//Boolean
+        }
+    }
+}
