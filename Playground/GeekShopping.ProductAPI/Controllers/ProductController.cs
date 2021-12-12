ae0751fc6e47181af2c53080d3618fc443c062c8
joaopjk <joaopjk48@gmail.com>
2021-12-12 16:43:06 -0300

    ProductController

---
+++ Playground/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> Find(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null)
+                return NotFound();
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            if (products == null)
+                return NotFound();
+            return Ok(products);
+        }
+    }
+}
