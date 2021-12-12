f408d5688b4c7c11d1663d438163ca138c93b624
joaopjk <joaopjk48@gmail.com>
2021-12-12 15:36:18 -0300

    Configurando a classe Product

--- Playground/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ Playground/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -5,9 +5,8 @@ namespace GeekShopping.ProductAPI.Model.Context
     public class MySQLContext : DbContext
     {
         public MySQLContext() { }
-        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
-        {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
-        }
+        public DbSet<Product> Products { get; set; }
     }
 }
