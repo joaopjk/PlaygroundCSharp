3fe1269f28199241abe0c354d6e3ccc6946a945f
joaopjk <joaopjk48@gmail.com>
2022-04-30 15:43:34 -0300

    Criando os domínios da API

--- Playground/Manager.Domain/Entities/User.cs
+++ Playground/Manager.Domain/Entities/User.cs
@@ -1,15 +1,62 @@
-﻿using System;
+﻿using Manager.Domain.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Manager.Domain.Entities
 {
     public class User : Base
     {
+
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Password { get; private set; }
+
+        protected User() { }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+        }
+
         public override bool Validate()
         {
-            throw new NotImplementedException();
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if (!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+
+                throw new Exception("Alguns campos estão inválidos, por favor corrigi-los: " + _errors[0]);
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+            Validate();
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
         }
     }
 }
