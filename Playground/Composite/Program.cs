033d0cbdfe81a43bc453c0e572d08ca8e50c5f9f
joaopjk <joaopjk48@gmail.com>
2022-05-16 21:08:41 -0300

    Composite

--- Playground/Composite/Program.cs
+++ Playground/Composite/Program.cs
@@ -4,9 +4,25 @@ namespace Composite
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] _)
         {
-            Console.WriteLine("Hello World!");
+            Organizacao organizacao = new() { Nome = "João Inc" };
+
+            Organizacao departamentoTI = new() { Nome = "TI" };
+            departamentoTI.Add(new Funcionario { Nome = "João", Horas = 44 });
+            departamentoTI.Add(new Funcionario { Nome = "Pedro", Horas = 44 });
+            departamentoTI.Add(new Funcionario { Nome = "Annie", Horas = 44 });
+
+            Organizacao departamentoRH = new() { Nome = "RH" };
+            departamentoRH.Add(new Funcionario { Nome = "Paulo", Horas = 44 });
+            departamentoRH.Add(new Funcionario { Nome = "Andre", Horas = 44 });
+            departamentoRH.Add(new Funcionario { Nome = "Marlom", Horas = 44 });
+
+            organizacao.Add(departamentoTI);
+            organizacao.Add(departamentoRH);
+
+            organizacao.GetHoraTrabalhada();
+            Console.ReadLine();
         }
     }
 }
