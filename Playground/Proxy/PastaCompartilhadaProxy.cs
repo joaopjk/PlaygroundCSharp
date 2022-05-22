ccebcb3b2e8b6b95cdea55676574c977f5f5972e
joaopjk <joaopjk48@gmail.com>
2022-05-22 17:20:41 -0300

    Proxy

---
+++ Playground/Proxy/PastaCompartilhadaProxy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Proxy
+{
+    public class PastaCompartilhadaProxy : IPastaCompartilhada
+    {
+        //Proxy
+        //Contém uma referência a classe RealSubject e pode acessar os membos da classe RealSubject conforme necessário.
+
+        private IPastaCompartilhada pasta;
+        private Funcionario funcionario;
+
+        public PastaCompartilhadaProxy(Funcionario funcionario)
+        {
+            this.funcionario = funcionario;
+        }
+
+        public void OperacaoDeLeituraGravacao()
+        {
+            if (funcionario.Perfil.ToUpper() == "CEO")
+            {
+                pasta = new PastaCompartilhada();
+                Console.WriteLine("Invocando a pasta real");
+                pasta.OperacaoDeLeituraGravacao();
+            }
+            else
+            {
+                Console.WriteLine("Você não tem permissão para usar a pasta.");
+            }
+        }
+    }
+}
