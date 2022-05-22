e93684cac444a19cf649b9d73aadcd5b9d016575
joaopjk <joaopjk48@gmail.com>
2022-05-22 03:17:36 -0300

    Facade

---
+++ Playground/Facade/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace Facade
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
---
+++ Playground/Facade/Program.cs
@@ -0,0 +1,17 @@
+﻿using Facade.Subsistema;
+using System;
+
+namespace Facade
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+            var concederEmprestimo = new CreditoFacade(new LimiteCredito(), new Serasa(), new Cadin(), new Cadastro());
+            var cliente = new Cliente("João Cícero");
+
+            concederEmprestimo.ConcederEmprestimo(cliente, 2000);
+            Console.ReadKey();
+        }
+    }
+}
