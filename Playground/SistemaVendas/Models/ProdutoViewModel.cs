26b807a2249739100976bc0158101257f45417a3
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2022-06-03 17:28:11 -0300

    Checkpoint

---
+++ Playground/SistemaVendas/Models/ProdutoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVendas.Models
+{
+    public class ProdutoViewModel
+    {
+        public int? Codigo { get; set; }
+        [Required(ErrorMessage = "Informe a descrição do Produto!")]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "Informe a quantidade do Produto!")]
+        public double Quantidade { get; set; }
+        [Required(ErrorMessage = "Informe o valor do Produto!")]
+        public decimal Valor { get; set; }
+        [Required(ErrorMessage = "Informe a Categoria do Produto!")]
+        public int? CodigoCategoria { get; set; }
+        public IEnumerable<SelectListItem> ListaCategorias { get; set; }
+    }
+}
