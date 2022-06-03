08f9dda1759ec198dbaeafcbafc095773173acbd
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2022-06-03 14:09:46 -0300

    Crud de Clientes

--- Playground/SistemaVendas/Entidades/Cliente.cs
+++ Playground/SistemaVendas/Entidades/Cliente.cs
@@ -8,7 +8,7 @@ namespace SistemaVendas.Entidades
         [Key]
         public int Codigo { get; set; }
         public string Nome { get; set; }
-        public string CPNJ_CPF { get; set; }
+        public string CNPJ_CPF { get; set; }
         public string Email { get; set; }
         public string Celular { get; set; }
         public ICollection<Venda> Vendas { get; set; }
---
+++ Playground/SistemaVendas/Models/ClienteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVendas.Models
+{
+    public class ClienteViewModel
+    {
+        public int? Codigo { get; set; }
+        [Required(ErrorMessage = "Informe o Nome do Cliente!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Informe o CNPJ/CPF do Cliente!")]
+        public string CNPJ_CPF { get; set; }
+        [Required(ErrorMessage = "Informe o Email do Cliente!")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Informe o Celular do Cliente!")]
+        public string Celular { get; set; }
+    }
+}
