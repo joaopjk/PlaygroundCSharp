eecd5211d5aa22e22ac5c46803a7e7f0c0d90294
joaopjk <joaopjk48@gmail.com>
2022-06-05 22:08:55 -0300

    checkpoint

---
+++ Playground/OpenClosed/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace OpenClosed
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
