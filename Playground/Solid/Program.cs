c27d9ae4d843331c00db94735563689a55a2a14b
joaopjk <joaopjk48@gmail.com>
2022-06-05 22:02:49 -0300

    SRP

---
+++ Playground/Solid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
