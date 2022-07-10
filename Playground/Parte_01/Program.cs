9e5994585a55f242302056fac004ac5554cb0530
joaopjk <joaopjk48@gmail.com>
2022-07-09 22:23:13 -0300

    Trabalhando com matrizes

---
+++ Playground/Parte_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Parte_01
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+            #region Matriz
+            string aulaIntro = "Introdução as coleçãos";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com conjuntos";
+            //string[] aulas = new string[]{ aulaIntro, aulaModelando, aulaSets };
+            //string[] aulas = new string[3] {aulaIntro,aulaModelando,aulaSets};
+            string[] aulas = new string[3];
+            aulas[0] = aulaIntro;
+            aulas[1] = aulaModelando;
+            aulas[2] = aulaSets;
+            foreach (string s in aulas) Console.WriteLine(s);
+            #endregion
+        }
+    }
+}
