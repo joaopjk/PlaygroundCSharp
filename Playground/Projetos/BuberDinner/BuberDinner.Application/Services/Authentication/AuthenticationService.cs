61e64ddb551359588e7f4955baadd8c27e2a56d0
joaopjk <joaopjk48@gmail.com>
2022-07-10 22:30:11 -0300

    ASP.NET 6 REST API Following CLEAN ARCHITECTURE & DDD Tutorial | Part 1 - Project Setup

---
+++ Playground/Projetos/BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿namespace BuberDinner.Application.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public AuthenticationResult Login(string email, string password)
+        {
+            return new AuthenticationResult(Guid.NewGuid(), "", "", email, Guid.NewGuid().ToString());
+        }
+
+        public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+        {
+            return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, Guid.NewGuid().ToString());
+        }
+    }
+}
