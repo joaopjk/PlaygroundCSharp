1b616c33db0aa60a3a4863a59012c2d15596c53c
joaopjk <joaopjk48@gmail.com>
2022-07-10 22:57:45 -0300

    JwtTokenGenerator

--- Playground/Projetos/BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ Playground/Projetos/BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,16 @@
-﻿namespace BuberDinner.Application.Services.Authentication
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
         public AuthenticationResult Login(string email, string password)
         {
             return new AuthenticationResult(Guid.NewGuid(), "", "", email, Guid.NewGuid().ToString());
@@ -9,7 +18,11 @@
 
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
-            return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, Guid.NewGuid().ToString());
+            Guid userId = Guid.NewGuid();
+
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+            return new AuthenticationResult(userId, firstName, lastName, email, token);
         }
     }
 }
