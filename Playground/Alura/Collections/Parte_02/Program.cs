c6e04dd82f76b77c3c8087b2bcd0aa1c0796257a
joaopjk <joaopjk48@gmail.com>
2022-07-18 17:52:31 -0300

    ArrayMultidimensional

--- Playground/Alura/Collections/Parte_02/Program.cs
+++ Playground/Alura/Collections/Parte_02/Program.cs
@@ -62,6 +62,39 @@ alunosHS.IntersectWith(outroConjunto);
 alunosHS.SymmetricExceptWith(outroConjunto);
 alunosHS.UnionWith(outroConjunto);
 #endregion
+#region ArrayMultidimensional
+string[,] resultados = new string[3, 3];
+//{
+//    {"Alemanha","Espanha","Itália" },
+//    {"Argentina","Holanda","França" },
+//    {"Holanda","Alemanha","Alemanha" }
+//};
+resultados[0, 0] = "Alemanha";
+resultados[1, 0] = "Argentina";
+resultados[2, 0] = "Holanda";
+
+resultados[0, 1] = "Espanha";
+resultados[1, 1] = "Holanda";
+resultados[2, 1] = "Alemanha";
+
+resultados[0, 2] = "Itália";
+resultados[1, 2] = "França";
+resultados[2, 2] = "Alemanha";
+//foreach (var item in resultados)
+//{
+//    Console.WriteLine(item);
+//}
+for (int copa = 0; copa < 3; copa++)
+{
+    int ano = 2014 - copa * 4;
+    Console.Write(ano.ToString().PadRight(12));
+}
+Console.WriteLine();
+for (int copa = 0; copa < 3; copa++)
+{
+    Console.Write(resultados[0, copa].PadRight(12));
+}
+#endregion
 class Aluno
 {
     public string Nome { get; set; }
