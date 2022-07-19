31d75fd6bf41aaf2caf104a597b592e8366acc6c
joaopjk <joaopjk48@gmail.com>
2022-07-19 20:35:21 -0300

    Trabalhando com coleções]

--- Playground/Alura/Collections/Parte_02/Program.cs
+++ Playground/Alura/Collections/Parte_02/Program.cs
@@ -5,7 +5,7 @@ IDictionary<string, Aluno> alunos = new Dictionary<string, Aluno>{
   {"VT", new Aluno("Vanessa", 34672)},
   {"AL", new Aluno("Ana", 5617)},
   {"RN", new Aluno("Rafael", 17645)},
-  {"WM", new Aluno("Wanderson", 11287)},
+  git s{"WM", new Aluno("Wanderson", 11287)},
 };
 foreach (var item in alunos)
 {
@@ -107,6 +107,27 @@ familias[0] = new string[] { "Fred", "Wilma", "Pedrita" };
 familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
 familias[2] = new string[] { "Florinda", "Kiko" };
 #endregion
+#region Consultando Collections
+List<Mes> meses = new()
+{
+    new Mes("Janeiro", 31),
+    new Mes("Janeiro", 31),
+    new Mes("Fevereiro", 28),
+    new Mes("Março", 31),
+    new Mes("Abril", 30),
+    new Mes("Maio", 31),
+    new Mes("Junho", 30),
+    new Mes("Julho", 31),
+    new Mes("Agosto", 31),
+    new Mes("Setembro", 30),
+    new Mes("Outubro", 31),
+    new Mes("Novembro", 30),
+    new Mes("Dezembro", 31)
+};
+IEnumerable<Mes> consultas = meses
+    .Where(x => x.Dias == 31)
+    .OrderBy(x => x.Nome);
+#endregion
 class Aluno
 {
     public string Nome { get; set; }
@@ -116,7 +137,6 @@ class Aluno
         Nome = nome;
         NumeroMatricula = numeroMatricula;
     }
-
     public override string ToString()
     {
         return $"[Aluno: {this.Nome} | Número Matrícula: {this.NumeroMatricula}";
@@ -128,5 +148,19 @@ class ComparadorMinusculo : IComparer<string>
     {
         //Fazer a comparação entre os itens sem case sensitive
         return string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
+class Mes
+{
+    public Mes(string nome, int dias)
+    {
+        Nome = nome;
+        Dias = dias;
+    }
+    public string Nome { get; private set; }
+    public int Dias { get; private set; }
+    public override string ToString()
+    {
+        return $"{Nome} - {Dias}";
     }
 }
