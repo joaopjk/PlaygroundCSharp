82c09369d0255ebf6bf949ab3ffb0b4df2f252aa
joaopjk <joaopjk48@gmail.com>
2022-07-19 22:02:06 -0300

    Domain.Core

---
+++ Playground/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publishe<T>(T @event) where T : Event;
+        void Subscribre<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
