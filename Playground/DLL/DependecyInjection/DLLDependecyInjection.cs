2600077acf34d832be089fb02a2b1364b6561d2b
joaopjk <joaopjk48@gmail.com>
2022-07-31 08:24:58 -0300

    Update database with EntityFramework

---
+++ Playground/DLL/DependecyInjection/DLLDependecyInjection.cs
@@ -0,0 +1,17 @@
+﻿using DLL.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DLL.DependecyInjection
+{
+    public static class DLLDependecyInjection
+    {
+        public static void AddDLLDependencies(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(_ =>
+            {
+                _.UseSqlServer("Server=127.0.0.1,1433;Database=CourseUdemy;user id=SA;Password=Root@123root");
+            });
+        }
+    }
+}
---
+++ Playground/DLL/Model/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DLL.Model
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
