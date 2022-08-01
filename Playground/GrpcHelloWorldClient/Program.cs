ed727fb3b7ff6a73e7b559408629741e222f7546
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2022-08-01 14:04:21 -0300

    gRPC

---
+++ Playground/GrpcHelloWorldClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using GrpcHelloWorldClient.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcHelloWorldClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new HelloService.HelloServiceClient(channel);
+
+            var reply = await client.SayHelloAsync(
+                    new HelloRequest { Name = "Hello SWN Client" }
+                );
+
+            Console.WriteLine("Greetings: " + reply.Message);
+        }
+    }
+}
