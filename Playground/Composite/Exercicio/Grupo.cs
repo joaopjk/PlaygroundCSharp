f08818c0e6e7df1c17187471daed563b897159c5
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2022-08-01 11:50:56 -0300

    Padrões de projeto

---
+++ Playground/Composite/Exercicio/Grupo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite.Exercicio
+{
+    //Composite
+    public class Grupo : ItemQuestionario
+    {
+        private IList<ItemQuestionario> _itens = new List<ItemQuestionario>();
+        public Grupo(string descricao) : base(descricao) { }
+
+        public override void Exibir()
+        {
+            Console.WriteLine($"Grupo: {Descricao}");
+            foreach (var item in _itens)
+            {
+                item.Exibir();
+            }
+        }
+        public void Add(ItemQuestionario item)
+        {
+            _itens.Add(item);
+        }
+        public void Remove(ItemQuestionario item)
+        {
+            _itens.Remove(item);
+        }
+    }
+}
