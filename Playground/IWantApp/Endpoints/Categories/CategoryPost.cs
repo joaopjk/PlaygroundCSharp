e02d5309d7839e29ef9985e4b299151de8e53a53
joaopjk <joaopjk48@gmail.com>
2022-08-01 19:32:31 -0300

    Configure first api method

---
+++ Playground/IWantApp/Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,16 @@
+﻿using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories
+{
+    public class CategoryPost
+    {
+        public static string Template => "/categories";
+        public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+        public static Delegate Handler => Action;
+
+        public static async Task<IResult> Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+        {
+            return Results.Ok();
+        }
+    }
+}
---
+++ Playground/IWantApp/Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApp.Infra.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Properties<string>()
+                .HaveMaxLength(100);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(_ => _.Name).IsRequired();
+            modelBuilder.Entity<Product>()
+                .Property(_ => _.Description).HasMaxLength(255);
+
+            modelBuilder.Entity<Category>()
+                .Property(_ => _.Name).IsRequired();
+        }
+    }
+}
