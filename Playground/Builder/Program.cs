a5ecb888947fb455bf9157315ce05d9d88f0ffe1
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2022-08-02 11:34:44 -0300

    Fluent Builder

--- Playground/Builder/Program.cs
+++ Playground/Builder/Program.cs
@@ -53,10 +53,11 @@ namespace Builder
             root.Name = rootName;
         }
 
-        public void AddChild(string childName, string childText)
+        public HtmlBuilder AddChild(string childName, string childText)
         {
             var e = new HtmlElement(childName, childText);
             root.Elements.Add(e);
+            return this;
         }
 
         public override string ToString()
@@ -94,8 +95,10 @@ namespace Builder
             #endregion
 
             var builder = new HtmlBuilder("ul");
-            builder.AddChild("li", "hello");
-            builder.AddChild("li", "world");
+            //builder.AddChild("li", "hello");
+            //builder.AddChild("li", "world");
+            builder.AddChild("li", "hello")
+                   .AddChild("li", "world");
             WriteLine(builder.ToString());
         }
     }
