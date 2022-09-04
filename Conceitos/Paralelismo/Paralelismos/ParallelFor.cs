4a21220304fab632c8371d545ea46a1ac0988523
joaopjk <joaopjk48@gmail.com>
2022-09-03 21:47:20 -0300

    Conceitos

---
+++ Conceitos/Paralelismo/Paralelismos/ParallelFor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Paralelismos
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            //TAREFA 1: Cozinhar e refogar EM SÉRIE
+            //TAREFA 2: Cozinhar e refogar EM PARALELO
+            //TAREFA 3: Medir o tempo dos 2 procedimentos
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            CozinharMacarrao();
+            RefogarMolho();
+            stopwatch.Stop();
+            Console.WriteLine("Sem parallel: {0}", stopwatch.ElapsedMilliseconds / 1000.0);
+
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            Parallel.Invoke(() => CozinharMacarrao(),
+                            () => RefogarMolho());
+            stopwatch.Stop();
+            Console.WriteLine("Com parallel: {0}", stopwatch.ElapsedMilliseconds / 1000.0);
+
+            Console.WriteLine(
+                "Retire do fogo e ponha o molho sobre o macarrão. " +
+                "Bom apetite! " +
+                "Tecle [ENTER] para terminar.");
+            Console.ReadLine();
+        }
+
+        static void CozinharMacarrao()
+        {
+            Console.WriteLine("Cozinhando macarrão...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Macarrão já está cozido!");
+            Console.WriteLine();
+        }
+
+        static void RefogarMolho()
+        {
+            Console.WriteLine("Refogando molho...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Molho já está refogado!");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Conceitos/Paralelismo/Paralelismos/ParallelFor_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Paralelismos
+{
+    class ParallelFor_2
+    {
+        static void Main(string[] _)
+        {
+            //Tarefa 1: Processar uma faixa de 100 itens em paralelo
+            //Tarefa 2: Completou sem interrupção?
+            //Tarefa 3: Interromper quando começar a processar o valor 75
+            //Tarefa 4: Quantos itens foram processados (parcialmente)?
+
+            //Tarefa 1:
+            ParallelLoopResult parallelLoopResult = Parallel.For(0, 99, (int i, ParallelLoopState state) =>
+            {
+                //Tarefa 3
+                if (i == 75)
+                    state.Break();
+                Processar(i);
+            });
+
+            //Tarefa 2
+            Console.WriteLine("Teminou o processo sem interrupção: {0}", parallelLoopResult.IsCompleted);
+            //Tarefa 4
+            Console.WriteLine("Quantos itens foram processados (parcialmente)? {0}", parallelLoopResult.LowestBreakIteration);
+            Console.WriteLine("Término do processamento. Tecle [ENTER] para terminar.");
+            Console.ReadLine();
+        }
+
+        static void Processar(object item)
+        {
+            Console.WriteLine("Começando a trabalhar com: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Terminando a trabalhar com: " + item);
+        }
+    }
+}
