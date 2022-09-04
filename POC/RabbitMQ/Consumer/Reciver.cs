7e3a55bb2293eeee341e2e8d92ef1a8784e3104e
joaopjk <joaopjk48@gmail.com>
2022-09-03 21:48:14 -0300

    RabbitMQ

---
+++ POC/RabbitMQ/Consumer/Reciver.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    internal class Reciver
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasisTest", false, false, false, null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.Span;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Recive message: " + message);
+                };
+                channel.BasicConsume("BasisTest", true, consumer);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ POC/RabbitMQ/Producer/Sender.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    internal class Sender
+    {
+        static void Main(string[] _)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasisTest", false, false, false, null);
+                string message = "Getting started with RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "BasisTest", null, body);
+            }
+            Console.ReadKey();
+        }
+    }
+}
