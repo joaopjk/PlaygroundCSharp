63c5fb10d536dac5361dc977a74413e3b20f38d5
joaopjk <joaopjk48@gmail.com>
2022-09-03 21:57:34 -0300

    Control statements

---
+++ Conceitos/ControlStatements/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ControlStatements
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            /* Control statements are used to control the program execution flow. Used to make execution flow "jump forward" or "jump
+             * backward".
+             * Conditional Control Statements: if(simple,else,nested-if)
+             * Looping Control: while | do-while | for
+             * Jumping Control: goto | break | continue
+             */
+        }
+    }
+}
