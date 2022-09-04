4f69bd8207af20c5494f2aba39304b330266511d
joaopjk <joaopjk48@gmail.com>
2022-09-03 21:26:03 -0300

    Factories

---
+++ DesignPatterns/Builders/Builder.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+  public class HtmlElement
+  {
+    public string Name, Text;
+    public List<HtmlElement> Elements = new();
+    private const int indentSize = 2;
+
+    public HtmlElement() { }
+
+    public HtmlElement(string name, string text)
+    {
+      Name = name ?? throw new ArgumentNullException(nameof(name));
+      Text = text ?? throw new ArgumentNullException(nameof(text));
+    }
+
+    private string ToStringImp(int indent)
+    {
+      var sb = new StringBuilder();
+      var i = new string(' ', indentSize * indent);
+      sb.AppendLine($"{i}<{Name}>");
+      if (!string.IsNullOrEmpty(Text))
+      {
+        sb.Append(new string(' ', indentSize * (indent + 1)));
+        sb.AppendLine(Text);
+      }
+      foreach (var e in Elements)
+      {
+        sb.Append(e.ToStringImp(indent + 1));
+      }
+      sb.AppendLine($"{i}</{Name}>");
+      return sb.ToString();
+    }
+
+    public override string ToString()
+    {
+      return ToStringImp(0);
+    }
+  }
+
+  class HtmlBuilder
+  {
+    private readonly string rootName;
+    HtmlElement root = new();
+    public HtmlBuilder(string rootName)
+    {
+      this.rootName = rootName;
+      root.Name = rootName;
+    }
+
+    public HtmlBuilder AddChild(string childName, string childText)
+    {
+      var e = new HtmlElement(childName, childText);
+      root.Elements.Add(e);
+      return this;
+    }
+
+    public override string ToString()
+    {
+      return root.ToString();
+    }
+
+    public void Clear()
+    {
+      root = new HtmlElement { Name = this.rootName };
+    }
+  }
+}
---
+++ DesignPatterns/Factories/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Factories
+{
+  public enum CoordinateSystem
+  {
+    Cartesian,
+    Polar
+  }
+
+  public class Point
+  {
+    private double x, y;
+
+    public Point(
+      double a,
+      double b,
+      CoordinateSystem coordinateSystem = CoordinateSystem.Cartesian)
+    {
+      switch (coordinateSystem)
+      {
+        case CoordinateSystem.Cartesian:
+          x = a;
+          y = b;
+          break;
+        case CoordinateSystem.Polar:
+          x = a * Math.Cos(b);
+          y = a * Math.Sin(b);
+          break;
+        default:
+          throw new ArgumentOutOfRangeException(
+            nameof(coordinateSystem),
+            coordinateSystem,
+            "");
+
+      }
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
