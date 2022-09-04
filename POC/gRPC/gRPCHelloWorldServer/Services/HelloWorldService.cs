c558e31753d64b5bfc5f378215756ce576d59583
joaopjk <joaopjk48@gmail.com>
2022-09-03 21:42:12 -0300

    gRPC

---
+++ POC/gRPC/gRPCHelloWorldServer/Services/HelloWorldService.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using gRPCHelloWorldServer.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPCHelloWorldServer.Services
+{
+    public class HelloWorldService : HelloService.HelloServiceBase
+    {
+        private readonly ILogger<HelloWorldService> _logger;
+
+        public HelloWorldService(ILogger<HelloWorldService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override Task<HelloResponse> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            _logger.Log(LogLevel.Information, request.ToString());
+            string resultMessage = $"Hello {request.Name}";
+            var response = new HelloResponse()
+            {
+                Message = resultMessage
+            };
+            return Task.FromResult(response);
+        }
+    }
+}
