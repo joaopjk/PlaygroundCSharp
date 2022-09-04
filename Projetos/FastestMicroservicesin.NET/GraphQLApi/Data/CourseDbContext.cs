12d0f9f05aeec5fd89d44c9111b998fa1441438d
joaopjk <joaopjk48@gmail.com>
2022-09-04 18:06:10 -0300

    Context for Api

---
+++ Projetos/FastestMicroservicesin.NET/GraphQLApi/Data/CourseDbContext.cs
@@ -0,0 +1,22 @@
+using GraphQLApi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLApi.Data
+{
+  public class CourseDbContext : DbContext
+  {
+    public CourseDbContext(DbContextOptions<CourseDbContext> options) : base(options) { }
+    public DbSet<Course> Course { get; set; }
+    public DbSet<Section> Section { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+    public DbSet<Lecture> Lecture { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Lecture>()
+        .HasDiscriminator<string>("Discriminator")
+        .HasValue<Subject>("Subject")
+        .HasValue<Assignment>("Assignment");
+    }
+  }
+}
--- Projetos/FastestMicroservicesin.NET/GraphQLApi/Startup.cs
+++ Projetos/FastestMicroservicesin.NET/GraphQLApi/Startup.cs
@@ -1,6 +1,8 @@
 using GraphiQl;
+using GraphQLApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +20,9 @@ namespace GraphQLApi
 
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<CourseDbContext>(
+        _ => _.UseSqlite("Data Source=Data\\coursedb.sqlite")
+      );
       services.AddMvc(options => options.EnableEndpointRouting = false);
     }
 
