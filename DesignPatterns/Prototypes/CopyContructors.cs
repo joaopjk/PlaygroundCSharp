b81b018548a23ec6cd33b0436fad23f81ff3d780
joaopjk <joaopjk48@gmail.com>
2022-09-07 06:47:57 -0300

    Explicit deep copy interface

--- DesignPatterns/Prototypes/CopyContructors.cs
+++ DesignPatterns/Prototypes/CopyContructors.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace Prototypes
 {
-  public class Person
+  public interface IPrototype<T>
+  {
+    T DeepCopy();
+  }
+
+  public class Person : IPrototype<Person>
   {
     private string[] names;
     private Address address;
@@ -13,14 +18,25 @@ namespace Prototypes
     public string[] Names { get => names; set => names = value; }
     public Address Address { get => address; set => address = value; }
 
+    public Person(string[] names, Address address)
+    {
+      this.names = names;
+      this.address = address;
+    }
+
     public Person(Person other)
     {
       names = other.Names;
       Address = new Address(other.Address);
     }
+
+    public Person DeepCopy()
+    {
+      return new Person(Names, Address.DeepCopy());
+    }
   }
 
-  public class Address
+  public class Address : IPrototype<Address>
   {
     private string streetName;
     private int houseNumber;
@@ -28,10 +44,21 @@ namespace Prototypes
     public string StreetName { get => StreetName; set => StreetName = value; }
     public int HouseNumber { get => houseNumber; set => houseNumber = value; }
 
+    public Address(string streetName, int houseNumber)
+    {
+      this.streetName = streetName;
+      this.houseNumber = houseNumber;
+    }
+
     public Address(Address other)
     {
       streetName = other.streetName;
       houseNumber = other.houseNumber;
+    }
+
+    public Address DeepCopy()
+    {
+      return new Address(StreetName, HouseNumber);
     }
   }
 }
