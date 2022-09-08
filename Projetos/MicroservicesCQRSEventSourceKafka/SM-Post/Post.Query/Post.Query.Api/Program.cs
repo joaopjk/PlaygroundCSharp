fe567c62ecc22367e8beea9d7ea2459be2eca535
joaopjk <joaopjk48@gmail.com>
2022-09-07 21:48:26 -0300

    Configure db context

--- Projetos/MicroservicesCQRSEventSourceKafka/SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ Projetos/MicroservicesCQRSEventSourceKafka/SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,19 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Post.Query.Infrastructure.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Action<DbContextOptionsBuilder> configureDbContext = (_ =>
+_
+  .UseLazyLoadingProxies()
+  .UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+builder.Services.AddDbContext<DatabaseContext>(configureDbContext);
+builder.Services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory(configureDbContext));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ Projetos/MicroservicesCQRSEventSourceKafka/SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Infrastructure.DataAccess
+{
+  public class DatabaseContext : DbContext
+  {
+    public DatabaseContext(DbContextOptions options) : base(options) { }
+    public DbSet<PostEntity> Posts { get; set; }
+    public DbSet<CommentEntity> Comments { get; set; }
+  }
+}
