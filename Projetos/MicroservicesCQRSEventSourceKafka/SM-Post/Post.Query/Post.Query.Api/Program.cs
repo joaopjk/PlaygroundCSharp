b626209045f45f6ad3243fe574d61de2e8c6ff4c
joaopjk <joaopjk48@gmail.com>
2022-09-08 19:25:53 -0300

    Event handler

--- Projetos/MicroservicesCQRSEventSourceKafka/SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ Projetos/MicroservicesCQRSEventSourceKafka/SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Handlers;
+using Post.Query.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,10 @@ _
   .UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 builder.Services.AddDbContext<DatabaseContext>(configureDbContext);
 builder.Services.AddSingleton(new DatabaseContextFactory(configureDbContext));
+
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
 
 //Create database and tables from code
 // var dataContext = builder.Services.BuildServiceProvider().GetRequiredService<DatabaseContext>();
