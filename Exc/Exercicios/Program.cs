835fca8fe52eaf75ca107e7b8cb603902cb5f8c6
joaopjk <joaopjk48@gmail.com>
2022-09-08 21:59:12 -0300

    Exc

---
+++ Exc/Exercicios/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+#region 01 - Volume de uma caixa retangular
+/*
+Calcular o volume de uma caixa retangular mostra de forma simples como declarar variáveis e manipular dados de entrada e saída via console
+O cálculo do volume é sempre dado pela multiplicação da altura (h), vezes a largura (L), vezes o comprimento (C).
+*/
+
+// double altura, largura, comprimento;
+
+// WriteLine("Digte a altura da caixa: ");
+// altura = Convert.ToDouble(ReadLine());
+
+// WriteLine("Digite a largura da caixa");
+// largura = Convert.ToDouble(ReadLine());
+
+// WriteLine("Digite o comprimento da caixa");
+// comprimento = Convert.ToDouble(ReadLine());
+
+// WriteLine($"O volume da caixa é: {altura * largura * comprimento}");
+
+#endregion
+
+#region 02 - Converter temperatura em graus Fahrenheit para Celsius
+//°C = (°F − 32) ÷ 1, 8
+
+// WriteLine("Digite a temperatura em Fahrenheit: ");
+// double fah = Convert.ToDouble(ReadLine());
+
+// WriteLine($"{fah}° Fahrenheit em Celsius: {(fah - 32) / 1.8}°");
+
+#endregion
+
+#region  03 - Calcula volume de um cilindro
+// const double pi = 3.1;
+
+// WriteLine("Digite a altura do cilindro");
+// double h = Convert.ToDouble(ReadLine());
+
+// WriteLine("Digite o raio do cilindro");
+// double r = Convert.ToDouble(ReadLine());
+
+// WriteLine($"O volume do cilindro em cm³ é: {pi * h * (Math.Pow(r, 2))}");
+
+
+#endregion
--- POC/gRPC/gRPCHelloWorldServer/Startup.cs
+++ POC/gRPC/gRPCHelloWorldServer/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace gRPCHelloWorldServer
 {
-  public class Startup
+  public static class Startup
   {
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
