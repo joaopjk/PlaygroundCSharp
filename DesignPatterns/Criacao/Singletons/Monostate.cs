233daaa5d73ef2c01d0c1a33e42ade99a0f26988
joaopjk <joaopjk48@gmail.com>
2022-09-21 14:50:49 -0300

    Monostate

---
+++ DesignPatterns/Criacao/Singletons/Monostate.cs
@@ -0,0 +1,16 @@
+namespace Singletons
+{
+  class CEO
+  {
+    private static string name;
+    private static int age;
+
+    public string Name { get => name; set => name = value; }
+    public int Age { get => age; set => age = value; }
+
+    public override string ToString()
+    {
+      return $"{nameof(Name)}: {Name} | {nameof(Age)}: {Age}";
+    }
+  }
+}
