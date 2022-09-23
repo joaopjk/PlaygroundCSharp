3d2b2079252055bea50ea4ef1cea75c2a447210d
joaopjk <joaopjk48@gmail.com>
2022-09-23 10:43:09 -0300

    Vector Raster

--- DesignPatterns/Estruturais/Adapters/VectorRaster.cs
+++ DesignPatterns/Estruturais/Adapters/VectorRaster.cs
@@ -39,4 +39,36 @@ namespace Adapters
       Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
     }
   }
+
+  class LineToPointAdapter : Collection<Point>
+  {
+    private static int count;
+
+    public LineToPointAdapter(Line line)
+    {
+      ++count;
+
+      int left = Math.Min(line.Start.x, line.End.x);
+      int right = Math.Max(line.Start.x, line.End.x);
+      int top = Math.Min(line.Start.y, line.End.y);
+      int bottom = Math.Max(line.Start.y, line.End.y);
+      int dx = right - left;
+      int dy = line.End.y - line.Start.y;
+
+      if (dx == 0)
+      {
+        for (int y = top; y <= bottom; y++)
+        {
+          Add(new Point(left, y));
+        }
+      }
+      else if (dy == 0)
+      {
+        for (int x = left; x <= right; ++x)
+        {
+          Add(new Point(x, top));
+        }
+      }
+    }
+  }
 }
