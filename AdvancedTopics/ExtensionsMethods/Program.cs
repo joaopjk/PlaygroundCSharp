ad7d94a768b2195a3cbc5e4da6aa9d25a7596b3e
joaopjk <joaopjk48@gmail.com>
2022-09-28 22:16:05 -0300

    Extensions methods

---
+++ AdvancedTopics/ExtensionsMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace ExtensionsMethods
+{
+  class Foo
+  {
+    public string Name => "Foo";
+  }
+
+  static class ExtensionsMethods
+  {
+    public static int Measure(this Foo foo)
+    {
+      return foo.Name.Length;
+    }
+
+    public static string ToBinary(this int n)
+    {
+      return Convert.ToString(n, 2);
+    }
+  }
+  static class Program
+  {
+    static void Main(string[] _)
+    {
+      var fooLength = new Foo().Measure();
+      WriteLine(fooLength.ToBinary());
+    }
+  }
+}
