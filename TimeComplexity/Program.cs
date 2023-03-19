3fbff1d2bf4f92fdc71c37ea9ef45885e52a2a0f
joaopjk <joaopjk48@gmail.com>
2023-03-19 01:57:35 -0300

    Time complexity

---
+++ TimeComplexity/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace TimeComplexity
+{
+    internal class Program
+    {
+        public class In
+        {
+            public static IEnumerable<int> ReadInts(string filePath)
+            {
+                using TextReader reader = new StreamReader(filePath);
+                string lastLine;
+
+                while ((lastLine = reader.ReadLine()) != null)
+                {
+                    yield return int.Parse(lastLine);
+                }
+            }
+        }
+        static void Main(string[] _)
+        {
+            var texts = new List<string>()
+            {
+                "1kints.txt",
+                "2kints.txt",
+                "4kints.txt",
+                "8kints.txt",
+                "16kints.txt",
+            };
+            foreach (var text in texts)
+            {
+                var ints = In.ReadInts(text).ToArray();
+                var watch = new Stopwatch();
+
+                watch.Start();
+                var triplets = ThreeSum.Count(ints);
+                watch.Stop();
+
+                Console.WriteLine(text);
+                Console.WriteLine($"Triplets: { triplets}");
+                Console.WriteLine($"Elapsed Time: {watch.Elapsed:g}");
+                Console.WriteLine("============================");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TimeComplexity/ThreeSum.cs
@@ -0,0 +1,25 @@
+﻿namespace TimeComplexity
+{
+    internal class ThreeSum
+    {
+        public static int Count(int[] nums)
+        {
+            int total = nums.Length;
+            int count = 0;
+
+            for (int i = 0; i < total; i++)
+            {
+                for (int j = i + 1; j < total; j++)
+                {
+                    for (int k = j + 1; k < total; k++)
+                    {
+                        if (nums[i] + nums[j] + nums[k] == 0)
+                            count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
