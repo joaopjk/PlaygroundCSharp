b1934b03d6a5dbd773df94e807e10e0cf9214648
Joao Cicero Vicente Sousa <joao.sousa@localiza.com>
2023-06-22 10:30:49 -0300

    Generics methods

--- CollectionsGenerics/Program.cs
+++ CollectionsGenerics/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections;
 using System.Diagnostics;
-using System.Linq;
 
 namespace CollectionsGenerics
 {
-    internal class Program
-    {
-        /*
+    /*
          * - Closely related data can be handled more efficiently when grouped together into a
          * collection. Instead of writing separete code to handle each individual object, you 
          * can use the same code to process all the elements of a collection.
@@ -65,6 +62,8 @@ namespace CollectionsGenerics
          * class in whici the capacity is always the same as the count. Elements are added to a bitarray by increasing
          * the lenght property, elements are deleted by decreasing the length property.
          */
+    internal class Program
+    {
         static void Main(string[] _)
         {
             ArrayList al = new(100) { 1, 2, 3, 4, "3", false, true };
@@ -150,7 +149,7 @@ namespace CollectionsGenerics
             Employee obj = (Employee)other;
 
             if (this.Id > obj.Id) return 1;
-            else if(this.Id < obj.Id) return -1;
+            else if (this.Id < obj.Id) return -1;
             return 0;
         }
 
@@ -200,7 +199,7 @@ namespace CollectionsGenerics
             IEnumerator en = alEmp.GetEnumerator();
 
             watch.Restart();
-            while(en.MoveNext())
+            while (en.MoveNext())
             {
                 /*
                  Propriedades BOF, EOF:
@@ -220,6 +219,222 @@ namespace CollectionsGenerics
             }
 
             alEmp.Reverse();
+        }
+    }
+
+    /*
+        Generics
+    - Generics introduce to the .Net Framework the concept of type parameters, which make it possible to design classes and methods
+    that defer the specification of one or more types until the class or method is declared and instantiated by client code. For
+    example, by using a generic type parameter T you can writ a single class that other client code can user without incurring the
+    cost or risk of runtime casts or boxing operations, as show here:
+    - A Generic Class is not specific to any particular Data type. But the instance of such a class would be specific to a given 
+    Data type mentioned at the time of creanting the instance/object that class.
+    - Generic collection classes are like Templates in C++. At the implementation level, the primary difference is that C# generic
+    type substituitions are performed at runtime and generic type information is thereby preserved for instantiated objects.
+
+        Advantages of Generics
+    - Type Safety: Generic types enforce type compliance at compile-time, and not run-time(as in the case of using Object). This
+    reduce the chances of data-type conflict during run-time.
+    - Peformance: The data types to be used in a Generic class are determined at compile-time, hence there is no need to perform type
+    casting(boxing and unboxing) during run-time, which is a computationally constly process.
+    - Code reuse: Since you only need to write the class once and customize it to use with the various data types, there is a 
+    substantial amount of code-reuse.
+
+
+        Tips
+    - When a generic type is first constructed with a value type as a parameter, the runtime creates a specializes generic type with
+    the supplied parameter substituted the appropriate places in the MSIL. Specialiez generic types are created once for each unique
+    value type used as a parameter.
+    - If at another point in your program code another GenericsList<T> class is created, this time with a different valu type such
+    as a long the runtime generates another version of the generic type, this time substituting a long in the appropriate place in
+    MSIL. Conversions are no longer necessary because each specialized generic class natively contains the value type.
+     */
+
+    class Stack
+    {
+        readonly int[] data;
+        int top = -1;
+        public Stack(int size)
+        {
+            data = new int[size];
+        }
+
+        public void Push(int value)
+        {
+            if (top < data.Length - 1)
+            {
+                top++;
+                data[top] = value;
+            }
+            else
+            {
+                Console.WriteLine("Não é possível mais adicionar elementos a Stack");
+            }
+        }
+
+        public int Pop()
+        {
+            if (top > -1)
+            {
+                int value = data[top];
+                data[top] = 0;
+                top--;
+                return value;
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        public int GetTopElement()
+        {
+            if (top > -1)
+            {
+                return data[top];
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        public void Prinf()
+        {
+            IEnumerator enumerator = data.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine((int)enumerator.Current);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Stack<T> where T : struct
+    {
+        readonly T[] data;
+
+        int top = -1;
+        public Stack(int size)
+        {
+            data = new T[size];
+        }
+
+        public void Push(T value)
+        {
+            if (top < data.Length - 1)
+            {
+                top++;
+                data[top] = value;
+            }
+            else
+            {
+                Console.WriteLine("Não é possível mais adicionar elementos a Stack");
+            }
+        }
+
+        public T Pop()
+        {
+            if (top > -1)
+            {
+                var value = data[top];
+                data[top] = default;
+                top--;
+                return value;
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        public T GetTopElement()
+        {
+            if (top > -1)
+            {
+                return data[top];
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        public void Prinf()
+        {
+            IEnumerator enumerator = data.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine((T)enumerator.Current);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class StackMain
+    {
+        static void Main(string[] _)
+        {
+            Console.Clear();
+
+            Stack stack = new(5);
+            stack.Push(5);
+            stack.Push(12);
+            stack.Push(14);
+            stack.Push(22);
+            stack.Push(134);
+
+            stack.Push(1222);
+
+            stack.Prinf();
+            stack.Pop();
+            stack.Push(1222);
+            stack.Prinf();
+
+            var stackGenericsDouble = new Stack<double>(5);
+            var stackGenericsDouble1 = new Stack<double>(5);
+
+            Console.WriteLine(stackGenericsDouble.GetType().GetHashCode());//  The same address memory
+            Console.WriteLine(stackGenericsDouble1.GetType().GetHashCode());// The same address memory
+
+            stackGenericsDouble.Push(5.1);
+            stackGenericsDouble.Push(12.123);
+            stackGenericsDouble.Push(14.22);
+            stackGenericsDouble.Push(22.12);
+            stackGenericsDouble.Push(134.12);
+
+            stackGenericsDouble.Push(1222);
+
+            stackGenericsDouble.Prinf();
+            stackGenericsDouble.Pop();
+            stackGenericsDouble.Push(1222);
+            stackGenericsDouble.Prinf();
+
+            //var stackEmp = new Stack<Employee>(5); Don't work. Stack can be create only with struct types
+        }
+    }
+
+    /*
+        Unbounded Type Parameters
+    Type parameters that have no constraints, such as T in public class SamplesClass<T>, are called unbounded type parameters.
+    Unbounded type parameters have the following rules:
+    - The != and == operators cannot be used because the is no guarantee that the concrete type arguments will suport these operators
+    - They can be converted to and from System.Object or explicitly converted to any interface type
+    - You can compare to null. If an unbounded parameter is compared to null, the comparison will always return false if the type
+    argument if the type argument is a value type.
+     */
+
+    class Demo
+    {
+        static void Swap<T>(ref T lhs, ref T rhs) where T: struct
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        static void Main(string[] _)
+        {
+            int a = 1;
+            int b = 2;
+
+            Swap<int>(ref a, ref b);
+            Console.WriteLine(a + "  " + b);
+
+            // You can also omit the type argument and the compiler will infer it. The Following call to Swap is equivalent to 
+            //the previous call
+            double d1 = 0.1, d2 = 0.2;
+            Swap(ref d1, ref d2);
+            Console.WriteLine(d1 + "  " + d2);
         }
     }
 }
